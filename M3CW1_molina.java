5a275ea2c71570c0982282c9dd4140400a05252f
NicMoe1982 <107799788+NicMoe1982@users.noreply.github.com>
2026-06-28 14:16:33 -0400

    Create M3CW1_molina.java with personal information
    Added a Java class that prints a name, course schedule, and programming experience statement.

---
+++ M3CW1_molina.java
@@ -0,0 +1,19 @@
+public class M3CW1_molina {
+    public static void main(String[] args) {
+
+       System.out.println("Nicholas"); // first name  
+       System.out.println("Molina"); // last name 
+
+        // title of following course schedule and course names with section numbers
+       System.out.println("2026 Fall Schedule"); 
+       System.out.println("CSC-134: C++ Programming, Section 1"); 
+       System.out.println("ENG-112: Writing/ Research in the Disciplines, Section 2"); 
+       System.out.println("MAT-171: Pre-Calculus Algebra, Section 1"); 
+
+        // programming experience statement
+System.out.println("My programming experience has been entirely self-taught, driven by curiosity and genuine interest. I've been programming for about three years and have hands-on experience with JavaScript, along with some exposure to Python. I've worked on a variety of projects, including web development, small business website audits, game development and mod debugging, and even some Visual Basic for Applications. I'm excited to continue learning in this course and to strengthen my programming skills.");
+
+
+
+    }
+}
